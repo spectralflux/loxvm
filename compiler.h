2c1f6ac0838527bf3dcfde183341dd60db7bfbfd
Simon Fullick <spectralflux@users.noreply.github.com>
2023-08-07 12:58:53 +0200

    added compiler

--- compiler.h
+++ compiler.h
@@ -1,6 +1,8 @@
 #ifndef LOXVM_COMPILER_H
 #define LOXVM_COMPILER_H
 
-void compile(const char* source);
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif //LOXVM_COMPILER_H
